dd8685b89b0f14df1088f708e4217414881e834a
Javeria Nadeem <javerianadeem03@gmail.com>
2019-10-29 03:27:02 -0700

    Project management app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import firebaseConfig from './config/firebaseConfig'
+//thunk is going to the project actions file
+//tthunk also returns a store enhancer beacuse it as a middleware
+// we can combine multiple enhancers using "compose"
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })
+    ),
+    // useFirestorefor profile is for syncing data to profile property
+    reactReduxFirebase(firebaseConfig, { useFirestoreForProfile: true, userProfile: 'users', attachAuthIsReady: true }),
+    reduxFirestore(firebaseConfig)
+  )
+)
+// using firebase auth ready so that firebase will load first then the component will load
+store.firebaseAuthIsReady.then(() => {
+  ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+})
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
